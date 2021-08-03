1f1b9797fa3fd94078cf14d91dd15ed92b96c73b
Laufire <laufiretechnologies@gmail.com>
2021-08-03 10:46:17 +0530

    Fixed some tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,11 @@
 import { React } from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-// NOTE: automock from jest config doesn't work on apps created with create-react-app.
-jest.mock('./components/simpleButton');
-jest.mock('./core/context', () => ({
-	state: {
-		count: 0,
-		refreshID: 'ABCD',
-	},
-}));
+jest.mock('./services/ticker');
+jest.mock('./components/game');
 
 import App from './App';
 
 test('renders learn react link', () => {
 	render(<App/>);
-	const someText = screen.getByText(/count/i);
-
-	expect(someText).toBeInTheDocument();
 });

5e6a4997c546e7fbd5f58dd9fffb067e8f9b3ce2
Laufire <laufiretechnologies@gmail.com>
2021-08-04 08:16:05 +0000

    Refactored a test to be sematically correct.

--- src/services/targetManager.test.js
+++ src/services/targetManager.test.js
@@ -4,11 +4,35 @@ import config from '../core/config';
 import { contains } from '@laufire/utils/collection';
 
 describe('TargetManager', () => {
+	const ant = {
+		type: 'ant',
+		id: '1234',
+		lives: 1,
+		score: 10,
+	};
+	const mosquito = {
+		type: 'mosquito',
+		id: '9876',
+		lives: 0,
+		score: 5,
+	};
+	const butterfly = {
+		type: 'butterfly',
+		lives: 1,
+		score: 0,
+	};
+
+	const targets = [
+		ant,
+		mosquito,
+		butterfly,
+	];
+
 	describe('getTarget', () => {
 		const { getTarget } = TargetManager;
 		const [x, y, type] = [0, 0, 'ant'];
 
-		test('getTarget returns a target', () => {
+		test('getTarget returns a new target', () => {
 			const target = getTarget({ x, y, type });
 			const typeConfig = config.targets[type];
 
@@ -16,32 +40,15 @@ describe('TargetManager', () => {
 				.toEqual(true);
 		});
 	});
+
 	describe('swatTarget', () => {
 		const { swatTarget } = TargetManager;
-		const ant = {
-			type: 'ant',
-			id: '1234',
-			lives: 1,
-		};
-		const mosquito = {
-			type: 'mosquito',
-			id: '9876',
-			lives: 0,
-		};
-		const butterfly = {
-			type: 'butterfly',
-			lives: 1,
-		};
 		const state = {
-			targets: [
-				ant,
-				mosquito,
-				butterfly,
-			],
+			targets: targets,
 			lives: 3,
 		};
 
-		test('swatTarget returns target lives', () => {
+		test('swatTarget reduces the life of the swatted target', () => {
 			const swattedAnt = { ...ant, lives: ant.lives - config.swatDamage };
 
 			const result = swatTarget(state, ant);
@@ -50,56 +57,49 @@ describe('TargetManager', () => {
 				.toMatchObject({ targets: [swattedAnt, mosquito, butterfly] });
 		});
 
-		test('swatTarget returns lives', () => {
-			const swattedButterfly = { ...butterfly,
-				lives: butterfly.lives - config.swatDamage };
+		test('swatTarget reduces player life when a butterfly is swatted',
+			() => {
+				const result = swatTarget(state, butterfly);
 
-			const result = swatTarget(state, butterfly);
-
-			expect(result).toMatchObject({ lives: state.lives
-				- config.penalDamage,
-			targets: [ant, mosquito, swattedButterfly] });
-		});
+				expect(result).toMatchObject({
+					lives: state.lives - config.penalDamage,
+				});
+			});
 	});
+
 	describe('getDeadTargets', () => {
 		const { getDeadTargets } = TargetManager;
-		const ant = {
-			lives: 1,
-		};
-		const mosquito = {
+		const deadTarget = {
+			...mosquito,
 			lives: 0,
 		};
 		const state = [
 			ant,
-			mosquito,
+			deadTarget,
 		];
 
-		test('addTarget returns a target', () => {
-			const result = getDeadTargets(state);
+		test('getDeadTargets returns all dead targets from the given targets',
+			() => {
+				const result = getDeadTargets(state);
 
-			expect(result)
-				.toEqual([mosquito]);
-		});
+				expect(result).toEqual([deadTarget]);
+			});
 	});
+
 	describe('getTargetsScore', () => {
 		const { getTargetsScore } = TargetManager;
-		const ant = {
-			score: 10,
-		};
-		const mosquito = {
-			score: 5,
-		};
 		const state = [
 			ant,
 			mosquito,
 		];
 		const data = 15;
 
-		test('addTarget returns a total score', () => {
-			const result = getTargetsScore(state);
+		test('getTargetsScore returns the total score of all given targets',
+			() => {
+				const result = getTargetsScore(state);
 
-			expect(result)
-				.toEqual(data);
-		});
+				expect(result)
+					.toEqual(data);
+			});
 	});
 });

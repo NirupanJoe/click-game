1696da42d8d26d9e8f353de0e95285bd27dc2bac
Laufire <laufiretechnologies@gmail.com>
2021-08-04 17:31:00 +0530

    Tested PositionService.getRandomY.

--- src/services/positionService.test.js
+++ src/services/positionService.test.js
@@ -1,9 +1,13 @@
+/* eslint-disable max-lines-per-function */
+jest.mock('@laufire/utils/random');
+
 import { range } from '@laufire/utils/collection';
 import { unique } from '../../test/helpers';
-import { getRandomX } from './positionService';
+import { rndBetween } from '@laufire/utils/random';
+import { getRandomX, getRandomY } from './positionService';
 
 describe('PositionService', () => {
-	test('getRandomX gives a rndBetween of width', () => {
+	test.skip('getRandomX gives a rndBetween of width', () => {
 		const width = 50;
 		const between = 1000;
 		const expectedCorrectness = 0.8;
@@ -16,5 +20,19 @@ describe('PositionService', () => {
 			expect(data >= min && data <= max).toEqual(true));
 		expect(unique(result).length)
 			.toBeGreaterThan(width * expectedCorrectness);
+	});
+
+	test('getRandomY delegates positioning to rndBetween', () => {
+		const heightRange = 50;
+		const min = 25;
+		const max = 75;
+		const mockValue = Symbol('mock');
+
+		rndBetween.mockImplementation(() => mockValue);
+
+		const result = getRandomY({ height: heightRange });
+
+		expect(rndBetween).toHaveBeenCalledWith(min, max);
+		expect(result).toEqual(mockValue);
 	});
 });

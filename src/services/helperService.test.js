5d92d292167004a694e7905b85ad26425dcd45e7
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-08-06 11:20:27 +0000

    Introduced test for isFuture function.

--- src/services/helperService.test.js
+++ src/services/helperService.test.js
@@ -1,19 +1,47 @@
+/* eslint-disable no-magic-numbers */
+/* eslint-disable max-lines-per-function */
 /* eslint-disable max-nested-callbacks */
 jest.mock('@laufire/utils/random');
 
 import config from '../core/config';
-import { getId } from './helperService';
+import * as helper from './helperService';
 import { rndString } from '@laufire/utils/random';
 
 describe('HelperService', () => {
-	test('getId gives a rndString of the configured idLength', () => {
-		const mockValue = Symbol('mock');
+	describe('getId', () => {
+		const { getId } = helper;
 
-		rndString.mockImplementation(() => mockValue);
+		test('getId gives a rndString of the configured idLength', () => {
+			const mockValue = Symbol('mock');
 
-		const result = getId();
+			rndString.mockImplementation(() => mockValue);
 
-		expect(rndString).toHaveBeenCalledWith(config.idLength);
-		expect(result).toEqual(mockValue);
+			const result = getId();
+
+			expect(rndString).toHaveBeenCalledWith(config.idLength);
+			expect(result).toEqual(mockValue);
+		});
+	});
+
+	describe('isFuture', () => {
+		const { isFuture, adjustTime } = helper;
+
+		test('isFuture returns true when input is less than new date',
+			() => {
+				const result = isFuture(adjustTime(
+					new Date(), -4, 'hours'
+				));
+
+				expect(result).toEqual(true);
+			});
+
+		test('isFuture returns false when input is greater than new date',
+			() => {
+				const result = isFuture(adjustTime(
+					new Date(), 4, 'hours'
+				));
+
+				expect(result).toEqual(false);
+			});
 	});
 });

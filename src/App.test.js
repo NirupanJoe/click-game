09a6891912966303cc682f5e72c42d931549e384
Laufire <laufiretechnologies@gmail.com>
2021-08-10 16:06:37 +0000

    Introduced rudimentary component testing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,23 @@
-import { React } from 'react';
+/* eslint-disable react/display-name */
+jest.mock('./components/game', () => () => <div role="game"/>);
+
+import React from 'react';
 import { render } from '@testing-library/react';
 
-jest.mock('./services/ticker');
-jest.mock('./components/game');
-
+import ticker from './services/ticker';
 import App from './App';
 
-test('renders learn react link', () => {
-	render(<App/>);
+beforeEach(() => {
+	jest.restoreAllMocks();
+});
+
+test('App renders the game', () => {
+	jest.spyOn(React, 'useEffect');
+	jest.spyOn(ticker, 'start').mockImplementation();
+
+	const { getByRole } = render(<App/>);
+
+	expect(React.useEffect).toHaveBeenCalledWith(ticker.start, []);
+
+	expect(getByRole('game')).toBeInTheDocument();
 });

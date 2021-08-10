849d8c45943ffebd241370f33f3bbfc63ea1bdc1
NirupanJoe <nirupanjoej@gmail.com>
2021-08-10 07:09:43 +0000

    Temp

--- src/components/activePowers.js
+++ src/components/activePowers.js
@@ -1,28 +1,18 @@
 import { React } from 'react';
+import config from '../core/config';
+import PowerManager from '../services/powerManager';
 import context from '../core/context';
-import { isFuture } from '../services/helperService';
 
 const style = {
 	height: 15,
 	width: 15,
 };
 
-const getPower = (power) =>
-	isFuture(power.powerType)
-	&& <img style={ style } src={ power.image }/> ;
+const getPower = (powerType) =>
+	<img style={ style } src={ config.powers[powerType].image }/> ;
 
 const ActivePowers = () => {
-	const getPowers = [
-		{
-			powerType: context.state.frozenTill,
-			image: context.config.powers.ice.image,
-		},
-		{
-			powerType: context.state.superTill,
-			image: context.config.powers.superBat.image,
-		},
-	];
-	const powers = getPowers.map(getPower);
+	const powers = PowerManager.getActivePowers(context).map(getPower);
 
 	return powers;
 };

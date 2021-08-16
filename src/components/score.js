f03dd6982948f3bb9b436b2f292fc02aaa99bf4d
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-08-16 08:19:45 +0000

    Refactored score test.

--- src/components/score.js
+++ src/components/score.js
@@ -3,12 +3,23 @@ import { React } from 'react';
 import context from '../core/context';
 import ActivePowers from './activePowers';
 
+const style = {
+	position: 'absolute',
+	top: '0%',
+	left: '0%',
+	background: 'silver',
+	fontSize: '4vw',
+	textAlign: 'center',
+};
+
+const getStyle = {
+	color: 'gold',
+	fontSize: '3vw',
+};
+
 const Score = () =>
-	<div
-		className="score"
-		role="score"
-	>
-		<Stars className="score-icon"/>
+	<div role="score" style={ style }>
+		<Stars role="icon" title="icon" style={ getStyle }/>
 		{ context.state.score }
 		{ ActivePowers() }
 	</div>;
--- src/components/score.test.js
+++ src/components/score.test.js
@@ -1,20 +1,28 @@
+/* eslint-disable react/display-name */
 jest.mock('../core/context', () => ({ state: { score: 10 }}));
+jest.mock('./activePowers', () => () =>
+	<div role="active-powers"><img key="a"/><img key="b"/></div>);
 
 import { render } from '@testing-library/react';
-import ActivePowers from './activePowers';
+import { React } from 'react';
+import context from '../core/context';
 import Score from './score';
 
 describe('Score', () => {
 	test('renders the component with appropriate styling', () => {
-		const component = render(Score()).getByRole('score');
+		const { getByRole } = render(Score());
+		const component = getByRole('score');
 
-		jest.mock('./activePowers', () => Symbol('img'));
-
-		expect(ActivePowers);
+		expect(getByRole('active-powers')).toBeInTheDocument();
+		expect(component).toHaveTextContent(context.state.score);
 		expect(component).toBeInTheDocument();
-		// expect(component).toHaveStyle({
-		// 	color: 'gold',
-		// 	fontSize: '3vw',
-		// });
+	});
+
+	test('renders the star component with appropriate styling', () => {
+		const { getByRole, getByTitle } = render(Score());
+		const component = getByTitle('icon');
+
+		expect(getByRole('active-powers')).toBeInTheDocument();
+		expect(component).toBeInTheDocument();
 	});
 });
